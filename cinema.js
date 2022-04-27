12e809990fb62a3c0810909b88ebf2c096cd2564
Jonatas <nunesjariel@gmail.com>
2022-04-26 21:08:00 -0300

    Refatorando codigo

--- cinema.js
+++ cinema.js
@@ -11,3 +11,23 @@ function listarTodosOsFilmes(filmes) {
   }
 }
 listarTodosOsFilmes(catalogoObj.data);
+
+function  listarFilmesEmCartaz(filmes){
+  const filmesEmCartaz = filmes.filter((filme)=> {
+    if (filme.emCartaz == true) {
+      return true;
+    }
+  });
+  return filmesEmCartaz;
+}
+
+
+// const listarFilmesEmCartaz = (filmes) =>
+//   filmes.filter((filme) => filme.emCartaz);
+
+function listarFilmesEmCartaz(filmes) {
+  return filmes.filter((filme) => filme.emCartaz);
+}
+console.log(listarFilmesEmCartaz(catalogoObj.data));
+
+
